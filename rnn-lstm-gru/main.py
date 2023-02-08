79db1085998d52e21a93aa0afca88d2c30f8dccd
Arielle Yoo <arielleyoo@campus-042-195.ucdavis.edu>
2023-02-07 16:10:19 -0800

    Fixed download issue

--- rnn-lstm-gru/main.py
+++ rnn-lstm-gru/main.py
@@ -18,6 +18,27 @@ sequence_length = 28
 hidden_size = 128
 num_layers = 2
 
+# Using code from download fix https://github.com/pytorch/vision/issues/1938
+from torchvision import datasets
+import torchvision.transforms as transforms
+import urllib
+
+num_workers = 0
+batch_size = 20
+basepath = './data'
+transform = transforms.ToTensor()
+
+def set_header_for(url, filename):
+    opener = urllib.request.URLopener()
+    opener.addheader('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36')
+    opener.retrieve(
+    url, f'{basepath}/{filename}')
+
+set_header_for('http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz', 'train-images-idx3-ubyte.gz')
+set_header_for('http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz', 'train-labels-idx1-ubyte.gz')
+set_header_for('http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz', 't10k-images-idx3-ubyte.gz')
+set_header_for('http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz', 't10k-labels-idx1-ubyte.gz')
+
 # MNIST dataset 
 train_dataset = torchvision.datasets.MNIST(root='./data', 
                                            train=True, 
